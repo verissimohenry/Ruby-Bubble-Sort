d5ad3d002f18c463a502918fc0daed138495882b
verissimohenry <verissimohenry04@gmail.com>
2021-03-25 14:21:38 +0100

    fix linter error

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,18 +1,18 @@
 def bubble_sort(arr)
   test = false
-    while !test
-  test = true
-            arr.each_with_index do |a, index|
-  next if index == arr.length - 1
+  until test
+    test = true
+    arr.each_with_index do |_a, index|
+      next if index == arr.length - 1
 
-            if arr[index] > arr[index+1]
-                arr[index], arr[index+1] = arr[index+1], arr[index] 
-                test = false
-            end
-        end
+      if arr[index] > arr[index + 1]
+        arr[index], arr[index + 1] = arr[index + 1], arr[index]
+        test = false
+      end
     end
+  end
 
-    arr
-end   
+  arr
+end
 arr = [4, 3, 78, 2, 0, 2]
 p bubble_sort(arr) # = [0, 2, 2, 3, 4, 78]
