88e21d05241b1d5476e6b91a40c3894ac5c5104d
verissimohenry <verissimohenry04@gmail.com>
2021-03-29 10:33:52 +0100

    bubble sort by solution

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -16,3 +16,22 @@ def bubble_sort(arr)
 end
 arr = [4, 3, 78, 2, 0, 2]
 p bubble_sort(arr) # = [0, 2, 2, 3, 4, 78]
+
+def bubble_sort_by(arr)
+  sorted = false
+  until sorted
+    sorted = true
+    arr.each_with_index do |item, index|
+      next unless arr[index + 1] && yield(item, arr[index + 1]).positive?
+
+      if arr[index] > arr[index + 1]
+        arr[index], arr[index + 1] = arr[index + 1], arr[index]
+        sorted = false
+      end
+    end
+  end
+
+  arr
+end
+
+p bubble_sort_by(%w[sabaka hi hello hey]) { |left, right| left.length - right.length }
